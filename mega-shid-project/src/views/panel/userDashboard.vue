7e76b1e8aa97c9d643f47a0b09cc648912598bcd
reza1899 <reza.pk.lu9@gmail.com>
2023-07-18 02:52:17 -0700

    add card to dashboard

--- mega-shid-project/src/views/panel/userDashboard.vue
+++ mega-shid-project/src/views/panel/userDashboard.vue
@@ -1,77 +1,81 @@
 <template>
-  <div>
-      <panelLayout>
+    <div>
+        <panelLayout>
+<!--            <div class="container">-->
+<!--                <div class="row">-->
+<!--                    <div class="col-md-6">-->
+<!--                        <newWave :value="82"></newWave>-->
+<!--                    </div>-->
+<!--                    <div class="col-md-6">-->
+<!--                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+            <select class="form-select w-25 m-3" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col"
+                     v-for="card in cardInfo"
+                     :key="card.id"
+                >
+                    <div class="card">
+                        <img src="#" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{card.name}}</h5>
+                            <p class="card-text">{{card.id}}</p>
+                        </div>
+                    </div>
+                </div>
 
-          <div class="container-fluid">
-              <div>
-                  <newWave :value="82"/>
-              </div>
-          </div>
-<!--          <div class="card" style="width: 18rem;">-->
-<!--              <img  class="card-img-top" alt="...">-->
-<!--              <div class="card-body">-->
-<!--                  <h5 class="card-title">Card title</h5>-->
-<!--                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-<!--                  <a href="#" class="btn btn-primary">Go somewhere</a>-->
-<!--              </div>-->
-<!--          </div>-->
-
-      </panelLayout>
-  </div>
+            </div>
+        </panelLayout>
+    </div>
 </template>
 
 <script>
-import newWave from '@/components/panel/waveform/newWave.vue'
-import  panelLayout from '@/layout/userPanel.vue'
+// import axios from "axios";
+// import newWave from '@/components/panel/waveform/newWave.vue'
+import panelLayout from '@/layout/userPanel.vue'
+
 export default {
-    name : "userDashboard",
-    components:{
-        newWave,
+    name: "userDashboard",
+    data() {
+        return {
+            cardInfo: {
+                card1 :  {
+                    name: "reza",
+                    id : null
+                },
+                card2 :  {
+                    name: "reza",
+                    id : null
+                },
+                card3 :  {
+                    name: "reza",
+                    id : null
+                },
+            }
+
+        }
+    },
+    // created() {
+    //         axios.get("").then ( (res) => {
+    //             console.log(res)
+    //         }).catch((error) => {
+    //             console.log(error)
+    //         })
+    // },
+    components: {
+        // newWave,
         panelLayout
     },
-    data(){
-        const products = [
-            {
-                id: 1,
-                name: 'Earthen Bottle',
-                href: '#',
-                price: '$48',
-                imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-                imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-            },
-            {
-                id: 2,
-                name: 'Nomad Tumbler',
-                href: '#',
-                price: '$35',
-                imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-                imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-            },
-            {
-                id: 3,
-                name: 'Focus Paper Refill',
-                href: '#',
-                price: '$89',
-                imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-                imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-            },
-            {
-                id: 4,
-                name: 'Machined Mechanical Pencil',
-                href: '#',
-                price: '$35',
-                imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-                imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-            },
-            // More products...
-        ]
-        return{
-            products
-        }
-    }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
